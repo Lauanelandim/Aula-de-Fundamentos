8ff3056cd4baa0a4e7ce185b94bf451651403522
Lauanelandim <landim0lauane@hotmail.com>
2024-08-29 21:32:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Olá, Mundo!");
+let preco = prompt("Digite um preço:");
+document.write(preco);
+
+let nome=prompt("Digite o seu nome:");
+document.write("Olá" ,nome,". Seja bem vindo ao site!");
+//olá, fulana. Seja bem vinda ao site!
